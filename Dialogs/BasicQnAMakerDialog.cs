87035512d40442afdf4a8d3094e3c3d7bf35f54d
Thumbass <kyladaky@gmail.com>
2017-11-24 10:20:37 -0500

    Update BasicQnAMakerDialog.cs

--- Dialogs/BasicQnAMakerDialog.cs
+++ Dialogs/BasicQnAMakerDialog.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -18,69 +18,73 @@ namespace LuisBot.Dialogs
         //Optional: defaultMessage, scoreThreshold[Range 0.0 – 1.0]
 
         public BasicQnAMakerDialog(string key, string id) : base(new QnAMakerService(new QnAMakerAttribute(key,
-            id, "No good match in FAQ.", 0.1))) { } 
+            id, "No good match in FAQ.", 0.1)))
+        { }
         protected override async Task RespondFromQnAMakerResultAsync(IDialogContext context, IMessageActivity message, QnAMakerResults result)
         {
             // answer is a string
+            var answer2 = message;
             var answer = result.Answers.First().Answer;
-            Activity reply = ((Activity)context.Activity).CreateReply();
+            Attachment data = new Attachment(answer);
+            answer2.Attachments.Add(data);
+            //Activity reply = ((Activity)context.Activity).CreateReply();
+            context.Done(answer2);
+        //    string[] qnaAnswerData = answer.Split(';');
+        //    int dataSize = qnaAnswerData.Length;
+        //    string title = qnaAnswerData[0];
+        //    string description = qnaAnswerData[1];
+        //    string url = qnaAnswerData[2];
+        //    string imageURL = qnaAnswerData[3];
 
-            string[] qnaAnswerData = answer.Split(';');
-            int dataSize = qnaAnswerData.Length;
-            string title = qnaAnswerData[0];
-            string description = qnaAnswerData[1];
-            string url = qnaAnswerData[2];
-            string imageURL = qnaAnswerData[3];
+        //    HeroCard card = new HeroCard
+        //    {
+        //        Title = title,
+        //        Subtitle = description,
+        //    };
+        //    if (title == "IT HelpDesk")
+        //    {
+        //        card.Buttons = new List<CardAction>
+        //            {
+        //                new CardAction(ActionTypes.OpenUrl, "Call", value: url)
+        //            };
+        //    }
+        //    else
+        //    {
+        //        card.Buttons = new List<CardAction>
+        //            {
+        //                new CardAction(ActionTypes.OpenUrl, "Learn More", value: url)
+        //            };
+        //    }
 
-            HeroCard card = new HeroCard
-            {
-                Title = title,
-                Subtitle = description,
-            };
-            if (title == "IT HelpDesk")
-            {
-                card.Buttons = new List<CardAction>
-                {
-                    new CardAction(ActionTypes.OpenUrl, "Call", value: url)
-                };
-            }
-            else 
-            {
-                card.Buttons = new List<CardAction>
-                {
-                    new CardAction(ActionTypes.OpenUrl, "Learn More", value: url)
-                };
-            }
+        //    card.Images = new List<CardImage>
+        //        {
+        //            new CardImage( url = imageURL)
+        //        };
+        //    reply.Attachments.Add(card.ToAttachment());
+        //    await context.PostAsync(reply);
+        //}
+        //protected override async Task DefaultWaitNextMessageAsync(IDialogContext context, IMessageActivity message, QnAMakerResults result)
+        //{
+        //    // get the URL
+        //    var answer = result.Answers.First().Answer;
+        //    string[] qnaAnswerData = answer.Split(';');
+        //    string qnaURL = qnaAnswerData[2];
 
-            card.Images = new List<CardImage>
-            {
-                new CardImage( url = imageURL)
-            };
-            reply.Attachments.Add(card.ToAttachment());
-            await context.PostAsync(reply);
-        }
-        protected override async Task DefaultWaitNextMessageAsync(IDialogContext context, IMessageActivity message, QnAMakerResults result)
-        {
-            // get the URL
-            var answer = result.Answers.First().Answer;
-            string[] qnaAnswerData = answer.Split(';');
-            string qnaURL = qnaAnswerData[2];
+        //    // pass user's question
+        //    var userQuestion = (context.Activity as Activity).Text;
+        //    context.Call(new FeedbackDialog(qnaURL, userQuestion), ResumeAfterFeedback);
+        //}
 
-            // pass user's question
-            var userQuestion = (context.Activity as Activity).Text;
-            context.Call(new FeedbackDialog(qnaURL, userQuestion), ResumeAfterFeedback);
-        }
-
-        private async Task ResumeAfterFeedback(IDialogContext context, IAwaitable<IMessageActivity> result)
-        {
-            if (await result != null)
-            {
-                await MessageReceivedAsync(context, result);
-            }
-            else
-            {
-                context.Done<IMessageActivity>(await result);
-            }
+        //private async Task ResumeAfterFeedback(IDialogContext context, IAwaitable<IMessageActivity> result)
+        //{
+        //    if (await result != null)
+        //    {
+        //        await MessageReceivedAsync(context, result);
+        //    }
+        //    else
+        //    {
+        //        context.Done<IMessageActivity>(await result);
+        //    }
         }
     }
 }
